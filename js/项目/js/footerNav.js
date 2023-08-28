03e4c726ed482ea59fa931df6aba6c82d8a3f896
王全佺 <506129326@qq.com>
2023-08-28 08:42:15 +0800

    20230828

---
+++ js/项目/js/footerNav.js
@@ -0,0 +1,46 @@
+document.write(`<div class="box_footer">
+<div class="box_left">
+    <div class="box_footer_nav">
+        <div class="footer_list">
+            <h3>关于我们</h3>
+            <a href="../公司简介.html">公司简介</a>
+            <a href="../专家.html">专家团队</a>
+            <a href="#">媒体报道</a>
+            <a href="#">交流合作</a>
+            <a href="../大事记.html">大 事 记</a>
+            <a href="#">道德规范</a>
+        </div>
+        <div class="footer_list">
+            <h3>资讯中心</h3>
+            <a href="#">资讯中心</a>
+            <a href="#">资讯中心</a>
+            <a href="#">言语科普</a>
+            <a href="#">网站地图</a>
+            <a href="#">郎朗SPEECH</a>
+        </div>
+        <div class="footer_list">
+            <h3>招贤纳士</h3>
+            <a href="#">中心地址</a>
+            <a href="#">人才招聘</a>
+            <a href="#">联系我们</a>
+        </div>
+        <div class="footer_list">
+            <h3>关注我们</h3>
+           <img src="../../项目/项目/imgs/二维码.png" alt="">
+
+        </div>
+        </div>
+        <div class="box_right">
+       
+        <div class="footer_list footer_list2">
+            <h1>400-000-6090</h1>
+            <i>服务邮箱：service@ostspeech.com</i><p>
+             <i>营业时间：周一至周日 8:30-18:00</i><p>
+              <i>总部地址：深圳市福田区莲花路2005号新世界文博中心33F3302室</i><p>
+               <i>直营中心：总部 香港 上海 广州 深圳 长沙 南京 佛山 东莞 重庆 杭州 苏州 合肥 青岛 成都 福州 郑州</i>
+                <i></i>
+        </div>
+        </div>
+
+    </div>
+</div>`)
---
+++ 项目/js/navbar.js
@@ -0,0 +1,57 @@
+// 导航头导入生成
+document.write(`<nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="../../项目/项目/index.html">
+                锦哲传媒
+            </a>
+        </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+            <ul class="nav navbar-nav navbar-right">
+                <li class="dropdown">
+                    <a href="#"
+                       class="dropdown-toggle"
+                       data-toggle="dropdown"
+                       role="button"
+                       aria-haspopup="true"
+                       aria-expanded="false">
+                        关于我们
+                        <span class="caret"></span>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a href="../../项目/项目/公司简介.html">公司简介</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="#">联系我们</a></li>
+                    </ul>
+                </li>
+                <li><a href="../../项目/项目/服务群体.html">服务群体</a></li>
+                <li><a href="../../项目/项目/专家.html">专家团队</a></li>
+                <li><a href="#">课程介绍</a></li>
+                <li class="dropdown">
+                    <a href="#"
+                       class="dropdown-toggle"
+                       data-toggle="dropdown"
+                       role="button"
+                       aria-haspopup="true"
+                       aria-expanded="false">
+                        新闻活动 <span class="caret"></span>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">公司新闻</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="#">语言科普</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div><!-- /.navbar-collapse -->
+    </div>
+</nav>`)
